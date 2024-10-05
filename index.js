eb3ba64e50b9e99988f137f08d9352a631795d92
Jhonatan Willian <73483588+JhonyFoxy@users.noreply.github.com>
2024-10-05 13:54:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+var minhaVariavel = "alguma coisa";
+const variavel = 3.14;
+let minhaNovaVariavel = "mudar";
+
+const nome = "Jhony";
+const sobrenome = "Foxy"
+let idade = 99;
+const nomeCompleto = nome + " " + sobrenome
+
+console.log("Olá, meu nome é " + nomeCompleto + " e tenho " + idade + " anos.");
+
+console.log(`Olá, meu nome é ${nomeCompleto} e tenho ${idade} anos.`);
+
+const tabuada = 8;
+let multiplicador = 1;
+
+console.log(`${tabuada} X 1 é: ${tabuada * multiplicador}`);
+multiplicador++;
+console.log(`${tabuada} X 1 é: ${tabuada * multiplicador}`);
+multiplicador++;
+console.log(`${tabuada} X 1 é: ${tabuada * multiplicador}`);
+multiplicador++;
+console.log(`${tabuada} X 1 é: ${tabuada * multiplicador}`);
+multiplicador++;
+console.log(`${tabuada} X 1 é: ${tabuada * multiplicador}`);
+multiplicador++;
+console.log(`${tabuada} X 1 é: ${tabuada * multiplicador}`);
+multiplicador++;
+console.log(`${tabuada} X 1 é: ${tabuada * multiplicador}`);
+multiplicador++;
+console.log(`${tabuada} X 1 é: ${tabuada * multiplicador}`);
+multiplicador++;
+console.log(`${tabuada} X 1 é: ${tabuada * multiplicador}`);
+multiplicador++;
+console.log(`${tabuada} X 1 é: ${tabuada * multiplicador}`);
+
+const nota1 = 8, nota2 = 7, nota3 = 3, nota4 =5;
+const media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+console.log(`A média do aluno é ${media}`)
+
